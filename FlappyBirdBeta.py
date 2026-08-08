61fe56313ee7b246671fa3281743d0b200895c44
SpringU26325 <3391147847@qq.com>
2026-08-08 22:31:14 +0800

    暂停框完善，暂停结束结束画面完成。自然结束画面缺失

--- FlappyBirdBeta.py
+++ FlappyBirdBeta.py
@@ -35,7 +35,6 @@ class Bird:
         self.y += 1
     def rise(self):
         self.y -= 1
-
 bird1 = Bird()
 
 # 水管
@@ -53,13 +52,35 @@ class CallCounter:
     def execute(self):
         self.count += 1
         self.count = self.count % WIDTH0
-
-
 counter = CallCounter()
 
+# 用户登录
+def user_login():
+    user_name = input('账号：')
+    user_login_flag = True
+    return user_name, user_login_flag
+
+# UI界面打印
+def ui_frame(tip_input):
+    ui_frame_temp = ''  # 待打印页面
+    game_over_tip = tip_input  # 提示词
+    blank_line = ' ' * (WIDTH + 2) + '\n' # 空白行
+    ui_frame = []  # 待打印页面参数
+    # 空白页
+    for j in range(HEIGHT):
+        ui_frame.append(blank_line)
+    # 分割线
+    ui_frame[0] = '-' * WIDTH
+    ui_frame[HEIGHT - 1] = '-' * WIDTH
+    # 中间提示
+    ui_frame[HEIGHT // 2] = blank_line[:WIDTH // 2 - len(game_over_tip) // 2] + \
+    game_over_tip +blank_line[WIDTH // 2 - len(game_over_tip) // 2 + len(game_over_tip):]
+    # 打印结束页面
+    for ui_frame_index in range(len(ui_frame)):
+        ui_frame_temp = ui_frame_temp + ui_frame[ui_frame_index]
+    print(ui_frame_temp)
 # 游戏
 def play():
-    pause = False
     game_flag = True
     pipes = [Pipe((WIDTH0 * k) - 2, random.randint(HEIGHT // 5, HEIGHT - HEIGHT // 5)) for k in range(4)]
     while game_flag:
@@ -74,12 +95,12 @@ def play():
             bird1.score += 1
 
         # 画面
-        information = [f'当前得分{bird1.score}', '按esc退出','按P暂停']
+        information = [f'{user_name}当前得分{bird1.score}', '按esc退出','按P暂停']
         separator = '---'
         separate_line = '-' * WIDTH
         information_line = separator
-        for i in range(len(information)):
-            information_line = information_line + information[i] + separator
+        for information_line_index in range(len(information)):
+            information_line = information_line + information[information_line_index] + separator
         def calculate_frame_parameters():
             nonlocal pipes,game_flag
             # 参数生成
@@ -91,13 +112,18 @@ def play():
                         if i == pipes[k].x or i == pipes[k].x - 1 or i == pipes[k].x + 1:
                             if not ((j == pipes[k].y) or (j == pipes[k].y - 1)  or (j == pipes[k].y + 1)):
                                 frames[j][i] = PIPE_IMAGE
+
+            # 管道碰撞检测
+            if frames[bird1.y][bird1.x] == PIPE_IMAGE:
+                game_flag = False
             frames[bird1.y][bird1.x] = bird1.image# 画小鸟
             # 画暂停画面
             pause_frame = copy.deepcopy(frames)
+            # H包裹
             for j in range(HEIGHT//4, HEIGHT - HEIGHT//4):
                 for i in range(WIDTH//4, WIDTH - WIDTH//4):
                     pause_frame[j][i] = 'H'
-
+            # 用空气填充内部
             for j in range(HEIGHT // 4 + 1, HEIGHT - HEIGHT // 4 - 1):
                 for i in range(WIDTH // 4 + 1, WIDTH - WIDTH // 4 - 1):
                     pause_frame[j][i] = AIR
@@ -115,12 +141,14 @@ def play():
             print(frame)
 
         def calculate_game_parameters():
+            nonlocal game_flag
             # 计算下一次参数
             counter.execute()  # 计时参数+1
             for i in range(len(pipes)):
                 pipes[i].x -= 1
             if bird1.y == HEIGHT - 1 or bird1.y == 0:
                 game_flag = False
+                return
             if counter.count %4 == 3:
                 bird1.fall()
 
@@ -131,27 +159,24 @@ def play():
 
             # 按P暂停
             if key == b'p' or key == b'P':
-                pause = True
-                # 创建暂停框
-
                 dot_count = 6
                 def calculate_pause_frame(dot_count_input, pause_frame_input):
                     # 将信息提示转换成字符串列表
                     tip_list = []
-                    tip_string = '[Q] Continue [C] Switch Account'
+                    tip_string = '[C] Continue [ESC] Exit'
                     for string_index in range(len(tip_string)):
                         tip_list.append(tip_string[string_index])
 
-                    # [Q] Continue [C] Switch Account
+                    # [C] Continue [ESC] Exit
                     for tip_list_index in range(len(tip_list)):
                         pause_frame_input[HEIGHT//2][WIDTH//2 - len(tip_list) // 2 + tip_list_index] = tip_list[tip_list_index]
                     # OOOOO
                     for dot in range(dot_count_input):
                         pause_frame_input[HEIGHT//2 + 1][WIDTH//2 - dot_count // 2 + dot] = 'O'  # 6
 
-
                 # 卡入循环
-                while 1:
+                pause_flag = True
+                while pause_flag:
                     pause_frame = calculate_frame_parameters()[1]
                     for dot_num in range(1, dot_count):
                         time.sleep(1)
@@ -160,12 +185,16 @@ def play():
                         if msvcrt.kbhit():
                             key = msvcrt.getch()
                             if key == b'\x1b':
-                               sys.exit(0)
-                            elif key == b'q' or key == b'Q':
-                                return
+                                game_flag = False
+                                pause_flag = False
+                                break
 
+                            elif key == b'c' or key == b'C':
+                                pause_flag = False
+                                break
 
-
+                            elif key == b't' or key == b'T':
+                                sys.exit(0)
 
             # 按空格跳跃
             elif key == b' ':
@@ -173,31 +202,28 @@ def play():
 
             # 按ESC结束
             elif key == b'\x1b':
-                game_over_frame_temp = ''  # 待打印页面
-                game_over_tip = f'GAME OVER !Your score is bird1: {bird1.score}'  # 提示词
-                blank_line = ' ' * WIDTH  # 空白行
-                game_over_frame = []  # 待打印页面参数
-                # 空白页
-                for j in range(HEIGHT):
-                    game_over_frame.append(blank_line)
-                # 分割线
-                game_over_frame[0] = separate_line
-                game_over_frame[HEIGHT - 1] = separate_line
-                # 中间提示
-                game_over_frame[HEIGHT // 2] = blank_line[:
-                WIDTH // 2 - len(game_over_tip) // 2] + game_over_tip +blank_line[WIDTH // 2 - len(game_over_tip) // 2 + len(game_over_tip):]
-                # 打印结束页面
-                for game_over_frame_index in range(len(game_over_frame)):
-                    game_over_frame_temp = game_over_frame_temp + game_over_frame[game_over_frame_index]
-                print(game_over_frame_temp)
+                game_over_tip = f'GAME OVER !Your score : {bird1.score}'
+                ui_frame(game_over_tip)
                 break
 
+            elif key == b't' or key == b'T':
+                sys.exit(0)
+
         # 画面参数计算
-        frames = calculate_frame_parameters()[0]  # 解包接受参数
+        frames = calculate_frame_parameters()[0]
         # 画面打印
         render_frame(frames)
         # 下一轮游戏参数计算
         calculate_game_parameters()
 
 if __name__ == '__main__':
-    play()
+    user_name, user_login_flag =user_login()
+    while user_login_flag:
+        time.sleep(1)
+        ui_frame('按S以开始，按ESC以结束')
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            if key.lower() == b's':
+                play()
+            elif key == b'\x1b':
+                sys.exit(0)
